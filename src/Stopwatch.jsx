cc6b9e2a59ef3a0bfad432f8dce1b7ad1a350992
jaga172023 <jaga.101602@gmail.com>
2024-05-08 21:37:57 +0530

    Initial Commit

---
+++ src/Stopwatch.jsx
@@ -0,0 +1,98 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+
+const Stopwatch = () => {
+	const [isTimerOn, setTimerOn] = useState(false);
+	const [myClock, setClock] = useState(0);
+
+	const handleClockStart = () => {
+		setTimerOn(!isTimerOn);
+		setClock((prevTime) => prevTime + 1);
+	};
+
+	const handleClockReset = () => {
+		setTimerOn(false);
+		setClock(0);
+	};
+
+	const convertIntoMinsSec = (timeInNumber) => {
+		let reminder = timeInNumber % 60;
+		let mins = Math.floor(timeInNumber / 60);
+
+		// console.log(reminder);
+
+		return `${mins < 10 ? "0" + mins : mins} : ${
+			reminder < 10 ? "0" + reminder : reminder
+		}`;
+	};
+
+	useEffect(() => {
+		let myTimer;
+
+		if (isTimerOn) {
+			myTimer = setInterval(() => {
+				setClock((prevTime) => prevTime + 1);
+			}, 1000);
+		}
+
+		return () => clearInterval(myTimer);
+	}, [myClock]);
+
+	return (
+		<section
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				// height: "100vh",
+			}}>
+			<article
+				style={{
+					width: "25rem",
+					height: "13rem",
+					border: "1px solid grey",
+					marginTop: "3rem",
+					borderRadius: "8px",
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "start",
+					alignItems: "center",
+				}}>
+				<h6 style={{ fontSize: "1.5rem", margin: "0", marginTop: "1rem" }}>
+					Stopwatch
+				</h6>
+
+				{/* <h4 style={{ fontSize: "1.5rem" }}>00 : 00</h4> */}
+				<h4 style={{ fontSize: "1.5rem" }}>{convertIntoMinsSec(myClock)}</h4>
+
+				<article
+					style={{
+						width: "100%",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}>
+					<button
+						style={{
+							marginRight: "1rem",
+							background: `${isTimerOn ? "red" : "green"}`,
+							color: "white",
+							padding: "1rem 2rem",
+							borderRadius: "4px",
+							fontSize: "1rem",
+						}}
+						onClick={() => handleClockStart()}>
+						{isTimerOn ? "Stop" : "Start"}
+					</button>
+					<button
+						onClick={() => handleClockReset()}
+						style={{ padding: "1rem 2rem" }}>
+						Reset
+					</button>
+				</article>
+			</article>
+		</section>
+	);
+};
+
+export default Stopwatch;
